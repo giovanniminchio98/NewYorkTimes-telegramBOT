b3170da55dcd1d5e0afc2d9b25f74544a254cf8b
giovanniminchio98 <69788614+giovanniminchio98@users.noreply.github.com>
2022-07-13 19:26:39 +0200

    updated

--- telegramBotNYT.py
+++ telegramBotNYT.py
@@ -1,83 +1,81 @@
-# general import needed
+# import os
+# import json
 import time
 import requests
+# import datetime
+# import dateutil
+# import pandas as pd
+# from dateutil.relativedelta import relativedelta
 import time
 
 # api for use NYT APi
 from pynytimes import NYTAPI
 
 # for telegram bot
-# telebotAPI are the best one i tested so far !!
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+# markup gen for telegram bot example
+def gen_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(InlineKeyboardButton("Yes", callback_data="cb_yes"),
+                               InlineKeyboardButton("No", callback_data="cb_no"))
+    return markup
+
 if __name__=='__main__':
 
-    # init telegram bot
-    TELEGRAM_TOKEN = 'YOUR TOKEN HERE'
+    # bot init Telegram
+    TELEGRAM_TOKEN = 'YOUR TELEGRAM BOT API'
     bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
     # NYT api settings
-    nyt = NYTAPI("YOUR NEW YORK TIMES APIs HERE", parse_dates=True)
+    nyt = NYTAPI("YOUR NYT API", parse_dates=True)
 
-    'top stories from the NYT'
+    # # top stories from the NYT
     # top_stories = nyt.top_stories()
     # a=1
 
-    'Get all the top stories from a specific category'
+    # # Get all the top stories from a specific category
     # top_science_stories = nyt.top_stories(section = "science")
     # b=1
 
-    'get most viewd articles of last day'
-    # most_viewed = nyt.most_viewed()
+    # get most viewd articles of last day
+    most_viewed = nyt.most_viewed()
+    c=1
 
-    'most shared article'
+    # # most shared article
     # most_shared = nyt.most_shared()
+    # d=1
 
-    # to get the latest articles published by NYT by category
+    # to get the latest articvle published by NYT
     sections = ['science','automobiles', 'business', 'climate',  'fashion','food','health', 'home page', 'new york','real estate', 'smarter living', 'sports','style','technology','the learning network','the upshot','today’s paper', 'travel','world','your money']
 
-    # save the last artcile time per each session, 
-    # if the new latest article is newer than we send it to the telegram bot (for now)
     latest_per_sections = []
 
-    # when the bot is started it just get all latest articles per category 
-    # and keep their unix timestamp
-    # (then when new articles come out it send them...)
     flag_first = True
 
-    # loop cycle (not best choice, still ALPHA version...)
     while True:
 
-        # for the session choosen (by me ahaha)
         for i in range(len(sections)):
-            try: # in case of exception/s...
-                #first scan
+            try: 
                 if flag_first:
                     latest = nyt.latest_articles(
                         source = "all",
                         section = sections[i]
-                    ) # this function return the latest 20 articles for the category
-                    # we just check the last one, but actually may happen that more article are new
-                    # is not rare to see more than onespawn at the same time
-
-                    # we base analysis on published date, possible also to check creation_date or updateed_date
+                    )
                     date = latest[0]['published_date']
-                    # convert datetime to timestamp
                     date_unix_timestamp =  int((time.mktime(date.timetuple())))
                     latest_per_sections.append(date_unix_timestamp)
 
-                    # to know when we have scanned all categories at least once
                     if len(latest_per_sections) == len(sections):
                         flag_first = False
 
-                # else we have to check if the last article is new or not
-                # new means "we haven't seen it yet"
                 else:
                     latest = nyt.latest_articles(
                         source = "all",
                         section = sections[i]
-                    ) 
+                    )
 
                     # check if article new, then keep it and notice about it
                     # date can be "updated_date" or "created_date" or "published_date""
@@ -85,65 +83,58 @@ if __name__=='__main__':
                     date_unix_timestamp =  int((time.mktime(date.timetuple())))
 
                     if date_unix_timestamp > latest_per_sections[i]:
-                        # new article for the category!!
-                        # print(sections[i], ' --> new article !!')
                         article = latest[0]
-
-                        ## for debug
-                        # print('\t Title: ', article['title'])
-                        # print('\t Abstract: ', article['abstract'])
-                        # print('\t\t source --> ', article['source'])
-
-                        # send message to telegram bot for testing
-                        # this is the message body (not used now)
-                        message_title = '*'+article['title']+'*'
+                        message_title = article['title']
                         message_abstract = article['abstract']
-                        message_image = requests.get(article['multimedia'][3]['url']).content
+                        flag_image = True
+                        try:
+                            message_image = requests.get(article['multimedia'][2]['url']).content
+                        except:
+                            flag_image = False
+
                         message_source = article['source']
-                        # bot.send_message(203287688, message_title, parse_mode= 'Markdown')
-
                         latest_per_sections[i] = date_unix_timestamp
-
-                        
-
-                        'to send message with image of article and text in caption'
-                        'actually useless for this bot since telegram preview handle everything for us...'
-                        # message_text = message_title + ' \n\n ' + message_abstract
-                        # try:
-                        #     message_image = requests.get(article['multimedia'][3]['url']).content
-                        # except:
-                        #     message_image = requests.get(article['thumbnail_standard']).content
-                        # bot.send_photo(203287688,message_image, caption=message_text, parse_mode= 'Markdown')
 
                         'to send message with just text and link to have preview on telegram'
                         'preview should be unlimited using telegram....testiong this theory'
-                        chat_id = 'YOUR CHAT ID AS INTEGER'
-                        try:
-                            article_url = 'section: *'+sections[i].upper()+'*' +'\n\n'+article['url']
-                            bot.send_message(chat_id, article_url, parse_mode='Markdown')
-                        except:
-                            text='*ERROR SENDING NEW ARTICLE !! *'
-                            bot.send_message(chat_id, text, parse_mode='Markdown')
 
-                       
+                        if article['url'].split('/')[3] == 'live':
+                            a=1
+                        else:
+                            try:
+                                #section
+                                mini_title = '<b>('+sections[i].upper()+') </b>' + ' \n\n'
 
-                        
+                                # title
+                                message_title = '<b>' + message_title + '</b>' 
+                                title_finale = "<a href='"+str(article['url'])+"'>"+ message_title +"</a>"+ '\n\n'
 
-                if (i+1)%10 == 0:
-                    # at most 10 resuqest per minute
-                    # since the categories are 20 we do a break every 10
-                    # each 1 minutes we check for new articles in all the categories
+                                # abstract
+                                message_abstract = '<b>Abstract: </b>'+ message_abstract +  ' \n'
+
+                                final_text = mini_title + title_finale + message_abstract + text #+ preview
+
+                                article_link = " <a href='"+str(article['url'])+"'> - </a> "
+
+                                section_link = "<a href='"+str("https://www.nytimes.com/section/"+sections[i].lower())+"'><b>"+ sections[i].upper() +"</b></a>"
+
+                                title_finale = 'New article'+article_link+section_link + "\n\n <a href='"+str(article['url'])+"'><b>Read More</b></a>"+ '\n\n'
+
+                                bot.send_message('CHANNEL NAME OR CHAT ID', title_finale, parse_mode='HTML', disable_notification=True)
+                            except:
+                                text='*ERROR SENDING NEW ARTICLE !! *'
+                                bot.send_message('CHANNEL NAME OR CHAT ID', text, parse_mode='Markdown')
+                                continue
+
+                if i%2 ==0 and i != 0:
+                    # at most 10 resuqest per minute AND 4000 PER DAY
+                    # since the categories are 50 we do a break every 10
+                    # each 5 minutes we check for new articles in all the categories
                     time.sleep(60)
-                    # actually the sleep has to be increased since is not enough (i reached daily limit with this conif...)
-            
-            # if exception thorwn print why
             except Exception as  e:
-
                 print('Eccezzione --> ', e)
+                bot.send_message('CHANNEL NAME OR CHAT ID', "<b>Errorebot...</b>controllare server !!! \n\n" + str(e), parse_mode='HTML')
                 if e.args[0] == 'Error 400: Invalid input':
                     # quanti-=1
                     a=1
                     sections.remove(sections[i])
-
-            
-    breakpoint() # never reached now dueto while loop
